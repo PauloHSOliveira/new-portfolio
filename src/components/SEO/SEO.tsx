b9453faa1431cb764e4987c531732b7b27902eb1
PauloHSOliveira <paulooliveiradev@gmail.com>
2023-05-04 20:52:58 -0300

    fix image

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -12,7 +12,7 @@ const SEO = ({ title, description }: SEOProps) => {
   const defaultTitle = 'Meu Site - Descrição do site aqui'
   const defaultDescription =
     'Este é um exemplo de descrição padrão. Este texto pode ser personalizado para cada página.'
-  const siteUrl = 'https://www.meusite.com.br'
+  const siteUrl = getCanonical()
 
   return (
     <NextSeo
@@ -26,7 +26,7 @@ const SEO = ({ title, description }: SEOProps) => {
         description: description || defaultDescription,
         images: [
           {
-            url: `${siteUrl}/static/developer.webp`,
+            url: `/static/developer.webp`,
             alt: title || defaultTitle,
           },
         ],
