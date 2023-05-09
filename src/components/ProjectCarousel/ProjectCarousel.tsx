be24237e1b4900dc7a3e9eb3edf7fd9f61a42454
PauloHSOliveira <paulooliveiradev@gmail.com>
2023-05-09 00:59:33 -0300

    fix slider

--- src/components/ProjectCarousel/ProjectCarousel.tsx
+++ src/components/ProjectCarousel/ProjectCarousel.tsx
@@ -29,13 +29,31 @@ const projects: Project[] = [
 
 const ProjectCarousel = () => {
   const router = useRouter()
+
   const handleProjectClick = (projectName: string) => {
     router.push(`/projects/${projectName}`)
   }
 
   return (
-    <div className="h-screen w-screen">
-      <AwesomeSlider>
+    <div className="h-screen w-screen overflow-hidden relative">
+      <AwesomeSlider
+        organicArrows={true}
+        selected={0}
+        fillParent={true}
+        infinite={true}
+        mobileTouch={true}
+        animation="cubeAnimation"
+        cssModule={{
+          slider: 'slider fluid-slider',
+          slide: 'slide fluid-slide',
+          bullet: 'bullet fluid-bullet',
+          active: 'active fluid-active',
+          visible: 'visible fluid-visible',
+          button: 'button fluid-button',
+          play: 'play fluid-play',
+          pause: 'pause fluid-pause',
+        }}
+      >
         {projects.map((project) => (
           <div
             key={project.name}
@@ -45,18 +63,18 @@ const ProjectCarousel = () => {
               className="h-full w-full bg-center bg-cover"
               style={{ backgroundImage: `url(${project.image})` }}
             >
-              <div className="h-full w-full bg-gray-900 bg-opacity-75 p-24">
-                <div className="flex flex-col justify-start h-full">
-                  <h1 className="text-4xl font-bold tracking-tight text-white sm:text-5xl md:text-6xl">
+              <div className="absolute inset-0 bg-gray-900 bg-opacity-75">
+                <div className="flex flex-col h-full items-start justify-end p-10 md:p-20">
+                  <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold tracking-tight text-white">
                     {project.name}
                   </h1>
-                  <p className="mt-3 text-base text-gray-300 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                  <p className="mt-3 sm:mt-5 md:mt-8 text-base sm:text-lg md:text-xl lg:text-2xl text-gray-300 max-w-lg">
                     {project.description}
                   </p>
-                  <div className="mt-auto">
+                  <div className="mt-8 md:mt-12">
                     <button
                       onClick={() => handleProjectClick(project.name)}
-                      className="btn-gray-900 btn border-none p-0 px-4 rounded-lg bg-gray-100 hover:bg-gray-50 shadow-lg inline-flex items-center justify-center text-base font-medium text-gray-900 hover:text-gray-900"
+                      className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
                     >
                       View Project
                       <FaArrowRight className="ml-2" />
