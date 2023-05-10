df26863a9741b254a0e4a87d162c359a311c6ea6
PauloHSOliveira <paulooliveiradev@gmail.com>
2023-05-10 11:10:27 -0300

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,28 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout, SEO } from '@/components'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import ReactGA from 'react-ga'
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    ReactGA.initialize(String(process.env.NEXT_PUBLIC_ANALITYCS_ID))
+
+    const handleRouteChange = (url: string) => {
+      ReactGA.set({ page: url })
+      ReactGA.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
   return (
     <Layout>
       <SEO
