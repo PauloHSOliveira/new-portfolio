8e81ec3cc8eb85b53ec0d3516e30a7f4e3fa724f
PauloHSOliveira <paulooliveiradev@gmail.com>
2023-05-11 09:58:43 -0300

    implement firebase analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,29 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout, SEO } from '@/components'
-import ReactGA from 'react-ga'
 import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { analytics } from '@/services/firebase'
+import { logEvent } from 'firebase/analytics'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
   useEffect(() => {
-    ReactGA.initialize(String(process.env.NEXT_PUBLIC_ANALYTICS_ID))
-    ReactGA.pageview(window.location.pathname + window.location.search)
-  }, [])
+    const handleRouteChange = (url: string) => {
+      if (analytics) {
+        logEvent(analytics, 'screen_view', { screen_name: url } as never)
+      }
+    }
+
+    // When the component is mounted, subscribe to router changes
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events, analytics])
 
   return (
     <Layout>
