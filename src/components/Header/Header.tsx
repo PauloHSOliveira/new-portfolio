5193db9ead8e6daff496e7f65541e0743141af40
PauloHSOliveira <paulooliveiradev@gmail.com>
2023-05-13 12:59:42 -0300

    added a ne image

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
 import { IBM_Plex_Mono } from 'next/font/google'
 import Link from 'next/link'
+import Image from 'next/image'
+import profilePicture from '/public/static/me.webp'
 
-const ibm = IBM_Plex_Mono({ subsets: ['latin'], weight: ['400'] })
+const ibm = IBM_Plex_Mono({ subsets: ['latin'], weight: ['400', '700'] })
 
 interface HeaderProps {
   openMenu: () => void
@@ -43,7 +45,16 @@ const Header = ({ openMenu }: HeaderProps) => {
     >
       <div className="flex justify-between items-center px-4 py-3 md:px-8 lg:px-16 xl:px-24">
         <Link href="/" className="text-xl font-bold">
-          PH Oliveira
+          <div className="flex gap-2 items-center justify-center">
+            <Image
+              src={profilePicture}
+              alt="Profile Picture"
+              width={40}
+              height={40}
+              className="shadow-lg rounded-2xl"
+            />
+            PH Oliveira
+          </div>
         </Link>
         <label htmlFor="menu-toggle" className="cursor-pointer lg:hidden">
           <svg
