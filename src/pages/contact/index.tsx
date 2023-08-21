3d05bc80e028c21f24f94aaf91a2512faa91f1d0
PauloHSOliveira <paulooliveiradev@gmail.com>
2023-08-20 22:35:14 -0300

    fix build and image optimization

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -14,7 +14,9 @@ function Contact() {
 
   return (
     <div className="max-w-4xl mx-auto py-24 px-4 md:px-4 md:my-8">
-      <h1 className="text-2xl md:text-4xl font-bold mb-4 text-black">Get in Touch</h1>
+      <h1 className="text-2xl md:text-4xl font-bold mb-4 text-black">
+        Get in Touch
+      </h1>
       <p className="text-gray-600 text-sm md:text-lg mb-8">
         Do you fancy saying hi to me or you want to get started with your
         project and you need my help? Feel free to contact me.
