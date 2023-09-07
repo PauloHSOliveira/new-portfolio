c022d87e371a0cd2b3f6ad90de8d9adcd4a49713
PauloHSOliveira <paulooliveiradev@gmail.com>
2023-09-07 15:38:46 -0300

    fix errors

--- functions/src/sendContactEmail.ts
+++ functions/src/sendContactEmail.ts
@@ -1,36 +1,36 @@
-import * as functions from 'firebase-functions'
-import { corsHandler } from './utils/corsHandler'
-import { ContactFormData } from './types'
-import sendContactEmail from './helpers/sendContactEmail'
+import * as functions from "firebase-functions"
+import { corsHandler } from "./utils/corsHandler"
+import { ContactFormData } from "./types"
+import sendContactEmail from "./helpers/sendContactEmail"
 
 export const sendContact = functions.https.onRequest((req, res) => {
   corsHandler(req, res, async () => {
     try {
-      const data = req.body.data as ContactFormData;
+      const data = req.body.data as ContactFormData
 
       if (!data || !data.name || !data.email || !data.message) {
         throw new Error(
-          'Invalid data. Please provide name, email, and message.'
-        );
+          "Invalid data. Please provide name, email, and message."
+        )
       }
 
-      const info = await sendContactEmail(data);
+      const info = await sendContactEmail(data)
 
       return res.status(200).json({
         data: {
-          message: 'Message sent successfully',
+          message: "Message sent successfully",
           messageId: info.messageId,
-        }
-      });
+        },
+      })
     } catch (error) {
-      functions.logger.error((error as any).response);
+      functions.logger.error((error as any).response)
       return res.status(500).json({
         data: {
-          message: 'Error sending email',
+          message: "Error sending email",
           error: (error as any).message,
-        }
-      });
+        },
+      })
     }
-  });
-});
+  })
+})
 
