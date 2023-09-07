4dbc131efa5a7f7cd3676cc45e551b0172f896dd
PauloHSOliveira <paulooliveiradev@gmail.com>
2023-09-07 14:20:10 -0300

    fix cors and function

--- src/services/sendContactEmail.ts
+++ src/services/sendContactEmail.ts
@@ -1,14 +1,21 @@
-import { functions } from './firebase'
-import { httpsCallable, HttpsCallableResult } from 'firebase/functions'
+import { functions } from './firebase';
+import { httpsCallable, HttpsCallableResult } from 'firebase/functions';
 
 export const sendContactEmail = async (data: any) => {
   try {
     const sendEmailFunction = httpsCallable<any, HttpsCallableResult<any>>(
       functions,
       'sendContact'
-    )
-    await sendEmailFunction(data)
+    );
+
+    // Call the function and await its response
+    const result = await sendEmailFunction(data);
+
+    // Handle the result as needed (e.g., check result.data, status, etc.)
+    console.log('Function Response:', result);
+
+    return result; // You can return the result or use it as needed
   } catch (error) {
-    throw new Error(error as any)
+    throw new Error(error as any);
   }
-}
+};
