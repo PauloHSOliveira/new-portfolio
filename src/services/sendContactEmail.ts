080d6617439d8487ed87dc5b84091bd05db9d769
PauloHSOliveira <paulooliveiradev@gmail.com>
2023-09-07 15:28:26 -0300

    fix functions

--- src/services/sendContactEmail.ts
+++ src/services/sendContactEmail.ts
@@ -8,14 +8,11 @@ export const sendContactEmail = async (data: any) => {
       'sendContact'
     );
 
-    // Call the function and await its response
-    const result = await sendEmailFunction(data);
+   await sendEmailFunction(data)
 
-    // Handle the result as needed (e.g., check result.data, status, etc.)
-    console.log('Function Response:', result);
 
-    return result; // You can return the result or use it as needed
   } catch (error) {
+    console.log(error)
     throw new Error(error as any);
   }
 };
