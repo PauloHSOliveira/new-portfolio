b57c7718b7bf8bc511a470f3d6333f8cddd6bb74
Paulo Oliveira <codephsoliveira@gmail.com>
2024-01-26 13:01:16 -0300

    add transform icons

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
   images: {
     domains: ['picsum.photos'],
   },
+  modularizeImports: {
+    '@phosphor-icons/react': {
+      transform: '@phosphor-icons/{{member}}',
+    },
+  },
   env: {
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
